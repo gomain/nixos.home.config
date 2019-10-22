577c0316d85a3d04e3ca229bab44850862fd2fa1
Joe Gomain Hoyes <gomain@gmail.com>
2019-10-22 11:45:27 +0700

    to smoke test: my-emacs

--- .config/nixpkgs/overlays/999-my-packages/default.nix
+++ .config/nixpkgs/overlays/999-my-packages/default.nix
@@ -1,9 +1,10 @@
 self: super:
 {
+  myEmacs = import ./my-emacs.nix { super };
   my-packages = with super; buildEnv {
     name = "my-packages";
     paths = [
-      
+      myEmacs
     ];
   };
 }
