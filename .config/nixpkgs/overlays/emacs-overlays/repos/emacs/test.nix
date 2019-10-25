1f4b8ee48365db1a8a517f0d4cd0ea10bfa67632
Joe Gomain Hoyes <gomain@gmail.com>
2019-10-25 23:47:29 +0700

    replace emacs-overlay with symlink

--- .config/nixpkgs/overlays/emacs-overlays/repos/emacs/test.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> { overlays = [ (import ../../default.nix) ]; } }:
-
-let
-  package = pkgs.emacsGit;
-  emacsPackages = pkgs.emacsPackagesNgGen package;
-  emacsWithPackages = emacsPackages.emacsWithPackages;
-in emacsWithPackages(epkgs: [ ])
