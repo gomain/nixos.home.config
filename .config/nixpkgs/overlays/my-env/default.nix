67ceb62cc441e0ba672a10f52848b08df2bfe8a3
Joe Gomain Hoyes <gomain@gmail.com>
2019-10-26 21:32:53 +0700

    emacs-overlay made relative symlink

--- .config/nixpkgs/overlays/my-env/default.nix
+++ .config/nixpkgs/overlays/my-env/default.nix
@@ -2,12 +2,24 @@ self: super:
 {
   my-nix-env = with self;
     let
-      myEmacs = super.callPackage ./my-emacs { };
+      myEmacs = with super; callPackage ./my-emacs { };
     in buildEnv {
       name = "my-nix-env";
       paths = [
+        xorg.appres
+        chromium
+        cypress
         myEmacs
+        expect
         keepassxc
+        nixfmt
+        nodejs-10_x
+        nodePackages.node2nix
+        nodePackages.npm
+        pciutils
+        unzip
+        xclip
+        zip
       ];
     };
 }
